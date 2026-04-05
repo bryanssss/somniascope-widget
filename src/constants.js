d241a4b70696fb424a84a340f81afb3846b20be8
Boyan <boyanminchev@yahoo.co.uk>
2026-04-05 12:46:02 +0100

    Create src/constants.js with default configuration constants and tone options

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+// Configuration Constants
+const CONFIG = {
+    API_URL: 'https://api.somnia.com',
+    TIMEOUT: 5000,
+    RETRY_COUNT: 3,
+};
+
+// Tone Options
+const TONE_OPTIONS = {
+    POSITIVE: 'positive',
+    NEUTRAL: 'neutral',
+    NEGATIVE: 'negative',
+};
+
+module.exports = { CONFIG, TONE_OPTIONS };
